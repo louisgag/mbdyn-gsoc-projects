7ec8a2c600fdc6a21d8a8d20e1c1605ce568c271
Louis Gagnon <louis.gagnon@polimi.it>
2018-01-25 09:28:52 +0100

    adding two entries (chances.md and install.md)

--- data/faq.js
+++ data/faq.js
@@ -1,5 +1,7 @@
 faq = {
 "What is this year's entry test?" : "entry_test_faq.md",
+  
+"What are my chances of being selected?" : "chances.md",
 
 "How to get in touch, familiarize with the project, and discuss possible activities?" : "get_in_touch.md",
 
@@ -12,4 +14,6 @@ faq = {
 "What NOT to ask for?" : "not_to_ask.md",
 
 "What are prospective applicants supposed to do?" : "applicants_todo.md",
+  
+"How do I compile and install MBDyn?" : "install.md",
 }
